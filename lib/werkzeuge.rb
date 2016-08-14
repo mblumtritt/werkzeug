f036ae4735f9b9ecaebc163f23875c6aa1971d57
Mike Blumtritt <mike.blumtritt@injixo.com>
2016-08-14 21:38:51 +0200

    Remove useless TOPLEVEL  definitions

--- lib/werkzeuge.rb
+++
@@ -1,6 +0,0 @@
-# includes all helper classes into TOPLEVEL context
-require_relative 'werkzeug'
-CustomExceptions = Werkzeug::CustomExceptions
-ThreadPool = Werkzeug::ThreadPool
-DataFile = Werkzeug::DataFile
-PidFile = Werkzeug::PidFile
