bd05e92a1cbc966611837cb5b60d3af05b25b88c
Mike Blumtritt <mike.blumtritt@injixo.com>
2017-01-28 23:39:18 +0100

    Extend Config
    Uses well initialized class variables now.
    Uses frozen string literal.

--- lib/werkzeug/config.rb
+++ lib/werkzeug/config.rb
@@ -1,24 +1,28 @@
+# frozen_string_literal: true
+
 module Werkzeug
   module Config
     class << self
       def default_thread_count=(count)
-        @default_thread_count = count
+        @@default_thread_count = count
       end
-      
+
       def default_thread_count
-        @default_thread_count || suggested_thread_count
+        @@default_thread_count || suggested_thread_count
       end
 
       def suggested_thread_count
-        @suggested_thread_count ||= find_suggested_thread_count
+        @@suggested_thread_count ||= find_suggested_thread_count
       end
-      
+
       private
-      
+
+      @@default_thread_count = @@suggested_thread_count = nil
+
       def find_suggested_thread_count
-        count = ENV['TC'.freeze].to_i
+        count = ENV['TC'].to_i
         return count unless count.zero?
-        require 'etc'.freeze unless defined?(Etc)
+        require 'etc' unless defined?(Etc)
         Etc.nprocessors
       end
     end
