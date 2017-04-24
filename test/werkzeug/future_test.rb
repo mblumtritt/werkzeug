54018d6b649da7702aa217f3de7ec208c44e3af4
Mike Blumtritt <mike.blumtritt@injixo.com>
2017-04-24 17:11:31 +0200

    Extend `FutureTest`

--- test/werkzeug/future_test.rb
+++ test/werkzeug/future_test.rb
@@ -2,9 +2,8 @@ require_relative '../test_helper'
 
 class FutureTest < Test
   def test_arguments
-    Werkzeug::Future.new(1, 2, arg1: 1, arg2: 2) do |*args|
-      assert_equal([1, 2, {arg1: 1, arg2: 2}], args)
-    end.wait
+    arguments = Werkzeug::Future.new(1, 2, arg1: 1, arg2: 2){ |*args| args }.value
+    assert_equal([1, 2, {arg1: 1, arg2: 2}], arguments)
   end
 
   def test_entities
@@ -21,20 +20,18 @@ class FutureTest < Test
     result = BasicObject.new
     subject = Werkzeug::Future.new{ result }
     assert_same(result, subject.value)
-    assert_same(result, subject.value) # re-read
     refute(subject.error?)
+    assert_same(result, subject.value) # re-read
   end
 
   def test_error_handling
     subject = Werkzeug::Future.new{ raise('invalid by definition') }
-    backtrace_top = nil
-    begin
-      subject.value
-    rescue StandardError => ex
-      backtrace_top = ex.backtrace[0]
-    end
+    execption = assert_raises(StandardError){ subject.value }
+    backtrace_top = execption.backtrace[0]
     assert(backtrace_top.index(__FILE__))
     assert_match(/in `block in test_error_handling'$/, backtrace_top)
     assert(subject.error?)
+    refute(subject.value?)
+    assert_raises(StandardError){ subject.value } # re-read
   end
 end
