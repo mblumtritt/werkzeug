c4d32b3643ef7a8b083245e26edb6a5365071edd
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2017-09-09 13:56:12 +0200

    Add tests for all `Error`

---
+++ test/werkzeug/error_test.rb
@@ -0,0 +1,30 @@
+require_relative '../test_helper'
+require_relative '../../lib/werkzeug/error'
+
+class ErrorTest < Test
+  ERROR_CLASSES = [
+      Werkzeug::Error::NoBlockGiven,
+      Werkzeug::Error::NoArgument,
+      Werkzeug::Error::ArgumentCount,
+      Werkzeug::Error::MethodExpected
+    ].freeze
+
+  def test_types
+    ([Werkzeug::Error] + ERROR_CLASSES).each do |error|
+      assert(error < ::ArgumentError)
+    end
+  end
+
+  def test_constants
+    ERROR_CLASSES.each do |error|
+      assert(defined?(error::MESSAGE_FORMAT))
+      assert(error::MESSAGE_FORMAT.is_a?(String))
+    end
+  end
+
+  def test_method
+    ERROR_CLASSES.each do |error|
+      assert_equal('method', defined?(error::raise!))
+    end
+  end
+end
