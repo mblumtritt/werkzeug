706aafc9c0efcae1aa1ea127e3a98db2bf6a9d5f
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2018-01-17 07:46:18 +0100

    Change parameter handling of SequenceFactory The SequenceFactory does not longer flatten the given elements. This allows sequences over Arrays as elements. (There was no real benefit to flatten given argument list.)

--- test/werkzeug/sequence_factory_test.rb
+++ test/werkzeug/sequence_factory_test.rb
@@ -2,41 +2,41 @@ require_relative '../test_helper'
 
 class SequenceFactoryTest < Test
   def test_loop
-    expectation = [1, 2, 3, 4, 1, 2, 3, 4, 1, 2]
-
     subject = Werkzeug::SequenceFactory.loop(1, 2, 3, 4)
+    expectation = [1, 2, 3, 4, 1, 2, 3, 4, 1, 2]
     assert_equal(expectation, Array.new(10, &subject))
 
-    subject = Werkzeug::SequenceFactory.loop(1, [2, 3], 4)
+    subject = Werkzeug::SequenceFactory.loop(1, [2, 3], [4])
+    expectation = [1, [2, 3], [4], 1, [2, 3], [4], 1, [2, 3], [4], 1]
     assert_equal(expectation, Array.new(10, &subject))
 
-    subject = Werkzeug::SequenceFactory.loop([1, [[2, 3], 4]])
-    assert_equal(expectation, Array.new(10, &subject))
+    subject = Werkzeug::SequenceFactory.loop
+    assert(Array.new(10, &subject).all?(&:nil?))
   end
 
   def test_ping_pong
-    expectation = [1, 2, 3, 4, 3, 2, 1, 2, 3, 4]
-
     subject = Werkzeug::SequenceFactory.ping_pong(1, 2, 3, 4)
+    expectation = [1, 2, 3, 4, 3, 2, 1, 2, 3, 4]
     assert_equal(expectation, Array.new(10, &subject))
 
     subject = Werkzeug::SequenceFactory.ping_pong(1, [2, 3], 4)
+    expectation = [1, [2, 3], 4, [2, 3], 1, [2, 3], 4, [2, 3], 1, [2, 3]]
     assert_equal(expectation, Array.new(10, &subject))
 
-    subject = Werkzeug::SequenceFactory.ping_pong([1, [[2, 3], 4]])
-    assert_equal(expectation, Array.new(10, &subject))
+    subject = Werkzeug::SequenceFactory.ping_pong
+    assert(Array.new(10, &subject).all?(&:nil?))
   end
 
   def test_random
-    expectation = [1, 2, 3, 4]
+    elements = [1, 2, 3, 4].freeze
+    subject = Werkzeug::SequenceFactory.random(*elements)
+    10.times{ assert_includes(elements, subject.call) }
 
-    subject = Werkzeug::SequenceFactory.random(1, 2, 3, 4)
-    10.times{ assert_includes(expectation, subject.call) }
+    elements = [1, [2, 3], 4].freeze
+    subject = Werkzeug::SequenceFactory.random(*elements)
+    10.times{ assert_includes(elements, subject.call) }
 
-    subject = Werkzeug::SequenceFactory.random(1, [2, 3], 4)
-    10.times{ assert_includes(expectation, subject.call) }
-
-    subject = Werkzeug::SequenceFactory.random([1, [[2, 3], 4]])
-    10.times{ assert_includes(expectation, subject.call) }
+    subject = Werkzeug::SequenceFactory.random
+    assert(Array.new(10, &subject).all?(&:nil?))
   end
 end
