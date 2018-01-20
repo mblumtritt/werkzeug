c48cb92a434ec7a711e54eaae58ab5b0a5c532af
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2018-01-20 20:58:36 +0100

    Fix shadow variable in PrefixedCalls

--- lib/werkzeug/prefixed_calls.rb
+++ lib/werkzeug/prefixed_calls.rb
@@ -1,9 +1,9 @@
 module Werkzeug
   module PrefixedCalls
     def call_all(prefix, *args)
-      @__prefixed_methods ||= Hash.new do |h, prefix|
-        prefix = prefix.to_s
-        prefixes = [prefix, '__' + prefix, '_' + prefix]
+      @__prefixed_methods ||= Hash.new do |h, prefx|
+        prefx = prefx.to_s
+        prefixes = [prefx, '__' + prefx, '_' + prefx]
         h[prefix.to_sym] = methods.select{ |name| name.to_s.start_with?(*prefixes) }.sort!
       end
       @__prefixed_methods[prefix.to_sym].each{ |name| send(name, *args) }
