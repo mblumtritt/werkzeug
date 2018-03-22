ddd40c92695bdbc925c5972a44bbbb670c3e2801
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2018-03-22 11:47:57 +0100

    Revert "Remove unused `minitest-proveit`"
    This reverts commit 531b1b93df4179312a14068cff46b7baa473c695.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,12 @@
 require 'minitest/autorun'
 require 'minitest/parallel'
+require 'minitest/proveit'
 
 require_relative '../lib/werkzeug'
 
 class Test < Minitest::Test
   parallelize_me!
+  prove_it!
 
   def assert_raises_message(type = nil, message, &block)
     execption = assert_raises(type, &block)
