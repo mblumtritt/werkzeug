a26b173ce03a7917314f2e04025c7e837f326b83
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2018-09-27 20:55:28 +0200

    Use singleton classes instead of modules

--- lib/werkzeug/config.rb
+++ lib/werkzeug/config.rb
@@ -1,30 +1,28 @@
 # frozen_string_literal: true
 
 module Werkzeug
-  module Config
-    class << self
-      def default_thread_count=(count)
-        @@default_thread_count = count
-      end
+  Config = Class.new do
+    attr_writer :default_thread_count
 
-      def default_thread_count
-        @@default_thread_count || suggested_thread_count
-      end
-
-      def suggested_thread_count
-        @@suggested_thread_count ||= find_suggested_thread_count
-      end
-
-      private
-
-      @@default_thread_count = @@suggested_thread_count = nil
-
-      def find_suggested_thread_count
-        count = ENV['TC'].to_i
-        return count unless count.zero?
-        require 'etc' unless defined?(Etc)
-        Etc.nprocessors
-      end
+    def initialize
+      @default_thread_count = @suggested_thread_count = nil
     end
-  end
+
+    def default_thread_count
+      @default_thread_count || suggested_thread_count
+    end
+
+    def suggested_thread_count
+      @suggested_thread_count ||= find_suggested_thread_count
+    end
+
+    private
+
+    def find_suggested_thread_count
+      count = ENV['TC'].to_i
+      return count unless count.zero?
+      require 'etc' unless defined?(Etc)
+      Etc.nprocessors
+    end
+  end.new
 end
--- lib/werkzeug/data_file.rb
+++ lib/werkzeug/data_file.rb
@@ -1,15 +1,32 @@
 require_relative 'error'
 
 module Werkzeug
-  module DataFile
+  DataFile = Class.new do
     RE_END = /^__END__$\s*/
 
-    def self.read(file_name = nil)
-      parse(File.read(file_name || caller_locations(1, 1)[0].path).split(RE_END, 2)[-1])
+    def initialize
+      @default = nil
     end
 
-    def self.parse(lines)
-      Error::MethodExpected.raise!(:each_line) unless lines.respond_to?(:each_line)
+    def default
+      @default ||= parse(defined?(::DATA) ? ::DATA : '').freeze
+    end
+
+    def names
+      default.keys
+    end
+
+    def [](name)
+      default[name]
+    end
+
+    def read(file_name = nil)
+      file_name ||= caller_locations(1, 1)[0].path
+      parse(File.read(file_name).split(RE_END, 2)[-1])
+    end
+
+    def parse(lines)
+      lines.respond_to?(:each_line) || Error::MethodExpected.raise!(:each_line)
       ret, name, content = {}, :default, []
       lines.each_line do |line|
         next content << line unless line.start_with?('@@ '.freeze)
@@ -19,19 +36,5 @@ module Werkzeug
       ret[name] = content.join
       ret
     end
-
-    def self.default
-      @@default ||= parse(defined?(::DATA) ? ::DATA : '').freeze
-    end
-
-    @@default = nil
-
-    def self.names
-      default.keys
-    end
-
-    def self.[](name)
-      default[name]
-    end
-  end
+  end.new
 end
