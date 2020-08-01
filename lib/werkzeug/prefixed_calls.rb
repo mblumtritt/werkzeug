689dbffc84a301d03d576998ebb3449fe21ddb41
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2020-08-01 14:32:12 +0200

    Allow blocks for PrefixedCalls

--- lib/werkzeug/prefixed_calls.rb
+++ lib/werkzeug/prefixed_calls.rb
@@ -1,10 +1,10 @@
 module Werkzeug
   module PrefixedCalls
-    def call_all(prefix, *args)
+    def call_all(prefix, *args, &block)
       prefix = prefix.to_s
       prefixes = [prefix, '__' + prefix, '_' + prefix]
       methods.select { |name| name.to_s.start_with?(*prefixes) }.sort!
-        .each { |name| send(name, *args) }
+        .each { |name| send(name, *args, &block) }
       self
     end
   end
