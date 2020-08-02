2933877c29784e76baf23658f31e88d608281c5b
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2020-08-02 14:32:57 +0200

    Allow method scoping with Delegate

--- lib/werkzeug/delegate.rb
+++ lib/werkzeug/delegate.rb
@@ -5,7 +5,7 @@ require_relative 'error'
 module Werkzeug
   module Delegate
     private def delegate(
-      *method_names, to:, prefix: false, private: false, allow_nil: false
+      *method_names, to:, prefix: false, scope: :public, allow_nil: false
     )
       Error::InvalidArgumentType.raise!(:to, to.inspect) unless to
       location = caller_locations(1, 1).first
@@ -16,7 +16,11 @@ module Werkzeug
         code += ".#{name}(...);end"
         module_eval(code, location.path, location.lineno)
       end
-      private(*method_names) if private
+      if scope == :private
+        private(*method_names)
+      elsif scope == :protected
+        protected(*method_names)
+      end
       method_names
     end
   end
--- test/werkzeug/delegate_test.rb
+++ test/werkzeug/delegate_test.rb
@@ -6,6 +6,8 @@ class DelegateTest < Test
 
     delegate :size, :[]=, to: :@ary
     delegate :empty?, to: :@ary, prefix: 'is'
+    delegate :to_a, to: :@ary, scope: :private
+    delegate :at, to: :@ary, scope: :protected
 
     attr_reader :ary
 
@@ -14,12 +16,24 @@ class DelegateTest < Test
     end
   end
 
-  def test_delegate
-    subject = SampleClass.new
+  def setup
+    @subject = SampleClass.new
+  end
 
+  attr_reader :subject
+
+  def test_delegate
     assert_same(42, subject[100] = 42)
     assert_same(42, subject.ary[100])
     assert_same(101, subject.size)
     refute(subject.is_empty?)
+  end
+
+  def test_scoping
+    refute_includes(SampleClass.public_instance_methods, :to_a)
+    refute_includes(SampleClass.public_instance_methods, :at)
+
+    assert_includes(SampleClass.private_instance_methods, :to_a)
+    assert_includes(SampleClass.protected_instance_methods, :at)
   end
 end
