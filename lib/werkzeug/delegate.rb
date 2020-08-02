4f382e8764ea53618269dbf70c0d82c29eeee6bc
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2020-08-02 09:57:32 +0200

    Add option 'allow_nil' to Delegate.delegate

--- lib/werkzeug/delegate.rb
+++ lib/werkzeug/delegate.rb
@@ -4,16 +4,17 @@ require_relative 'error'
 
 module Werkzeug
   module Delegate
-    private def delegate(*method_names, to:, prefix: false, private: false)
+    private def delegate(
+      *method_names, to:, prefix: false, private: false, allow_nil: false
+    )
       Error::InvalidArgumentType.raise!(:to, to.inspect) unless to
       location = caller_locations(1, 1).first
       prefix = prefix ? "#{prefix == true ? to : prefix}_" : ''
       method_names.each do |name|
-        module_eval(
-          "def #{prefix}#{name}(...);#{to}.#{name}(...);end",
-          location.path,
-          location.lineno
-        )
+        code = "def #{prefix}#{name}(...);#{to}"
+        code += '&' if allow_nil
+        code += ".#{name}(...);end"
+        module_eval(code, location.path, location.lineno)
       end
       private(*method_names) if private
       method_names
