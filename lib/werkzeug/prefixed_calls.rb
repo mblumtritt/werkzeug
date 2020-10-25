0569a4c5a56f233d7be26f31b492038f73aa84a4
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2020-10-25 08:46:34 +0100

    Allow prefixed calls with named parameters

--- lib/werkzeug/prefixed_calls.rb
+++ lib/werkzeug/prefixed_calls.rb
@@ -1,10 +1,10 @@
 module Werkzeug
   module PrefixedCalls
-    def call_all(prefix, *args, &block)
+    def call_all(prefix, *args, **opts, &block)
       prefix = prefix.to_s
       prefixes = [prefix, '__' + prefix, '_' + prefix]
       methods.select { |name| name.to_s.start_with?(*prefixes) }.sort!
-        .each { |name| send(name, *args, &block) }
+        .each { |name| send(name, *args, **opts, &block) }
       self
     end
   end
